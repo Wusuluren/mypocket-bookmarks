014bbe78bc17385a7a5ae97259a550986c33728e
wusuluren <weixiaoyiri@gmail.com>
2018-09-01 10:16:42 -0400

    fix upload on first start

--- upload.sh
+++ upload.sh
@@ -2,13 +2,15 @@
 
 monitorfile=readme.md
 
+git commit -m 'start' $monitorfile
+git push origin
 lastsize=$(du -b $monitorfile | awk '{print($1)}')
 while [ true ]
 do
     cursize=$(du -b $monitorfile | awk '{print($1)}')
     if [ $cursize -ne $lastsize ]; then
 	echo $(date), $cursie, $lastsize
-	git commit -m 'fix' $monitorfile
+	git commit -m 'update' $monitorfile
         git push origin
         lastsize=$cursize
     fi
